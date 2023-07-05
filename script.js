856632ad2ecf43377b443996b59af718dfc5d459
Sivan Mehta <sivanmehta@microsoft.com>
2023-07-05 16:54:21 -0700

    we now have 2 shaders

--- script.js
+++ script.js
@@ -5,21 +5,12 @@ const resolution = new THREE.Vector2(
   window.innerHeight
 );
 
-let mouseUp = true;
-
-document.addEventListener('mousedown', () => {
-  mouseUp = false;
-});
-document.addEventListener('mouseup', () => {
-  mouseUp = true;
-});
-
-class Animation {
-  constructor() {
+class Grayness {
+  constructor(id) {
     // scene renderer
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setSize(window.innerWidth, window.innerHeight, window.devicePixelRatio);
-    document.body.appendChild(this.renderer.domElement);
+    document.getElementById(id).appendChild(this.renderer.domElement);
     this.scene = new THREE.Scene();
     this.plane_geometry = new THREE.PlaneGeometry(2, 1.2);
     
@@ -28,10 +19,6 @@ class Animation {
     
     // image loader
     this.loader = new THREE.TextureLoader();
-
-    // setup splash listener
-    this.lastClick = new THREE.Vector2(0, 0);
-    document.addEventListener('mousemove', this.mouseInteraction.bind(this));
   }
 
   async init() {
@@ -44,7 +31,6 @@ class Animation {
         time: { value: 1 },
         resolution: { value: resolution },
         u_texture: { value: texture },
-        last_click: { value: this.lastClick }
       },
       fragmentShader
     });
@@ -76,10 +62,75 @@ class Animation {
   }
 }
 
+class Blur {
+  constructor(id) {
+    // scene renderer
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize(window.innerWidth, window.innerHeight, window.devicePixelRatio);
+    document.getElementById(id).appendChild(this.renderer.domElement);
+    this.scene = new THREE.Scene();
+    this.plane_geometry = new THREE.PlaneGeometry(2, 1.2);
+    
+    // animation variables
+    this.start = Date.now() / 1000;
+    
+    // image loader
+    this.loader = new THREE.TextureLoader();
+  }
+
+  async init() {
+    const fragmentShader = await fetch('./fragmentShader.glsl').then(res => res.text());
+    const texture = await new THREE.TextureLoader()
+      .loadAsync('https://sivanmehta.github.io/taco/taco-on-the-perch.png');
+
+    this.cat_material = new THREE.ShaderMaterial({
+      uniforms: {
+        time: { value: 1 },
+        resolution: { value: resolution },
+        u_texture: { value: texture },
+      },
+      fragmentShader
+    });
+    const cat_mesh = new THREE.Mesh(this.plane_geometry, this.cat_material);
+    cat_mesh.position.set(0,0,0);
+    this.scene.add(cat_mesh);
+    
+    this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.001, 1000);
+    this.camera.position.set(0, 0, 1);
+    this.scene.add(this.camera);
+  }
+
+  tick() {
+    const now = Date.now() /1000 - this.start;
+    this.cat_material.uniforms.time.value = now;
+    this.renderer.render(this.scene, this.camera);
+    requestAnimationFrame(this.tick.bind(this));
+  }
+
+  mouseInteraction(event) {
+    if(mouseUp) return
+    this.lastClick = new THREE.Vector2(
+      event.clientX,
+      // because 0,0 is in the top left in the browser and bottom right in the GPU?
+      window.innerHeight - event.clientY
+    );
+    this.start = Date.now() / 1000;
+    this.cat_material.uniforms.last_click.value = this.lastClick;
+  }
+}
+
+const animations = [{
+  id: 'grayness',
+  animation: Grayness
+}, {
+  id: 'blur',
+  animation: Blur
+}]
+
 async function start() {
-  const animation = new Animation();
-  await animation.init();
-  animation.tick();
+  const renders = animations.map(({ id, animation }) => new animation(id));
+  await Promise.all(renders.map(render => render.init()));
+  renders.forEach(render => render.tick());
 }
 
 start();
